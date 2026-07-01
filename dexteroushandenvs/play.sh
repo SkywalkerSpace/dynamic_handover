81983747534a365f1184391b41d0251b7978b90a
SkywalkerSpace <dzmbbs@qq.com>
2026-07-01 22:46:00 +0800

    add sh

---
+++ dexteroushandenvs/play.sh
@@ -0,0 +1,1 @@
+python train.py --task=AllegroHandDynamicHandover --algo=mappo --num_envs=20 --seed 22 --play --model_dir=logs/allegro_hand_dynamic_handover/mappo/models_seed22/3000 --headless
---
+++ dexteroushandenvs/train.sh
@@ -0,0 +1,1 @@
+python train.py --task=AllegroHandDynamicHandover --algo=mappo --num_envs=5120 --seed 22 --headless 
